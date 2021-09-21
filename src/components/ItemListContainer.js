401179d31717b786465a7dd5c6d5c07f53ac821c
Lorena De Luca <lore.1526@hotmail.com>
2021-09-20 22:19:13 -0300

    add cards & cartWidget

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,20 @@
+import './ItemListContainer.css';
+
+const ItemListContainer = () => {
+ 
+    return (
+    <div class="container-items">
+        <div class="item">
+            item 1
+        </div>
+        <div class="item">
+            item 2
+        </div>
+        <div class="item">
+            item 3  
+        </div>
+    </div>   
+     )
+}
+
+export default ItemListContainer;
