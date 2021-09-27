fd13168d2a348af727cc11f6b3db8c293db400bc
Lorena De Luca <lore.1526@hotmail.com>
2021-09-27 19:30:20 -0300

    add changes

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,37 +1,43 @@
 import './ItemCount.css';
+import React, {  useState } from "react";
 import Button from '@mui/material/Button';
 
 const ItemCount = ( {stock, initial, onAdd}) =>{
+    const [contador, setContador] = useState(initial); 
+  //  const [stockInternal, setStockInternal] = useState(stock); 
+    
+    const decreaseStock = () =>{
+        if(contador >= initial ){
+            setContador(contador + 1);
+        }
+    }
+
+    const addStock = () =>{
+        if (contador > initial ){
+            setContador(contador - 1);
+         //   setStockInternal(stock +1);
+        }
+    }
+    
     return (
         <>
-          <div className="item-picture" ></div>
-        <div className="item-footer">
-            <div className="item-footer-carrito">
-                <button className="item-footer-minus" onClick={ () => Decrease()}>-</button>
-                <span className="item-number">{initial}</span>
-                <button className="item-footer-plus" onClick={ () => AddStock()}>+</button>
+            <div className="item-picture" ></div>
+            <div className="item-footer">
+                <div className="item-footer-carrito">
+                    <Button l={2} className="item-footer-minus" variant="outlined" onClick={ () => addStock()}>-</Button>
+                    <span className="item-number">{contador}</span>
+                    <Button m={2} className="item-footer-plus"  onClick={ () => decreaseStock()} variant="outlined">+</Button>
+                </div>
+                <div className="item-footer-carrito">
+                    <Button className="item-agregar-carrito" variant="outlined">Agregar Carrito</Button>
+                </div>
             </div>
-            <div className="item-footer-carrito">
-                <Button mt={2} variant="text">Agregar Carrito</Button>
-            </div>
-        </div>
         </>
     );
+
 }
+
 
 export default ItemCount;
 
 
-const Decrease = () =>{
-  
-    if (this.initial > this.stock){
-        this.initial--;
-    }
-}
-
-const AddStock = () =>{
-  
-    if (this.initial < this.stock){
-        this.initial++;
-    }
-}
